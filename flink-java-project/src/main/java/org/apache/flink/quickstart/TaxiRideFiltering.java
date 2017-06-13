3b4c7723d7a483d4510cbea95457c9340de101d6
varshakirani <varshakirani@gmail.com>
2017-06-13 18:36:15 +0200

    KafkaConnection

--- flink-java-project/src/main/java/org/apache/flink/quickstart/TaxiRideFiltering.java
+++ flink-java-project/src/main/java/org/apache/flink/quickstart/TaxiRideFiltering.java
@@ -3,11 +3,18 @@ package org.apache.flink.quickstart;
 import com.dataartisans.flinktraining.exercises.datastream_java.datatypes.TaxiRide;
 import com.dataartisans.flinktraining.exercises.datastream_java.sources.TaxiRideSource;
 import com.dataartisans.flinktraining.exercises.datastream_java.utils.GeoUtils;
+import com.dataartisans.flinktraining.exercises.datastream_java.utils.TaxiRideSchema;
 import org.apache.flink.api.common.JobExecutionResult;
 import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer010;
+import org.apache.flink.streaming.util.serialization.DeserializationSchema;
+import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
+
+import java.io.IOException;
 
 /**
  * Created by Varsha Kirani on 6/6/2017.
@@ -26,9 +33,38 @@ public class TaxiRideFiltering {
         DataStream<TaxiRide> rides = env.addSource(new TaxiRideSource("E:\\Work\\DFKI\\FlinkLearning\\nycTaxiRides.gz",maxDelay,servingSpeed));
 
         DataStream<TaxiRide> filteredRides = rides.filter(new checkInNYC());
-        filteredRides.writeAsText("e:\\work\\dfki\\flinklearning\\flink-java-project\\src\\main\\resources\\output.txt");
+
+        //write data Kafka
+        //filteredRides.addSink(new FlinkKafkaProducer010<TaxiRide>(
+//                "localhost:9092", //default broker
+  //              "taxiRide",               //topic Id
+    //            new TaxiRideSchema1()
+      //          ));
+
+        //write to file
+        //filteredRides.writeAsText("e:\\work\\dfki\\flinklearning\\flink-java-project\\src\\main\\resources\\output.txt");
         env.execute();
 
+    }
+
+    public static class TaxiRideSchema1 implements DeserializationSchema <TaxiRide> {
+
+        @Override
+        public TaxiRide deserialize(byte[] bytes) throws IOException {
+            return null;
+        }
+
+        @Override
+        public boolean isEndOfStream(TaxiRide taxiRide) {
+            return false;
+        }
+
+
+
+        @Override
+        public TypeInformation getProducedType() {
+            return null;
+        }
     }
 
     public static class checkInNYC implements FilterFunction<TaxiRide> {
