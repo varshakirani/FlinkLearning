226c611b828bfe5a3be9dc0e4cde6bad7b5d378e
varshakirani <varshakirani@gmail.com>
2017-06-15 17:35:41 +0200

    ,

--- flink-java-project/src/main/java/org/apache/flink/quickstart/TimeTaxiRideState.java
+++ flink-java-project/src/main/java/org/apache/flink/quickstart/TimeTaxiRideState.java
@@ -1,6 +1,11 @@
 package org.apache.flink.quickstart;
 
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.common.time.Time;
+import org.apache.flink.runtime.executiongraph.restart.RestartStrategy;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by varsha on 15.06.17.
@@ -12,6 +17,15 @@ public class TimeTaxiRideState {
         //set up Stream environment
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
+        //enabling checkpointing for every 1s
+        env.enableCheckpointing(1000);
+
+        //restart strategy
+        env.setRestartStrategy(
+                RestartStrategies.fixedDelayRestart(6, Time.of(10, TimeUnit.SECONDS))
+        );
+
+        
 
     }
 }
